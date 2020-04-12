49a091e107f77945b26354af4d07f17d69c2efaf
David Dong <david.dong@fingerprints.com>
2020-04-12 17:35:24 +0800

    code submit / funciton .raw -> Hex array
    Translated the specified text format into common Hex array, outputted a standard C header file.

---
+++ MxtCfgConverter/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MxtCfgConverter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.importBtn = new System.Windows.Forms.Button();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.statusTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.BackColor = System.Drawing.Color.White;
+            this.fileNameTextBox.Location = new System.Drawing.Point(24, 41);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.ReadOnly = true;
+            this.fileNameTextBox.Size = new System.Drawing.Size(405, 20);
+            this.fileNameTextBox.TabIndex = 0;
+            // 
+            // importBtn
+            // 
+            this.importBtn.Location = new System.Drawing.Point(464, 38);
+            this.importBtn.Name = "importBtn";
+            this.importBtn.Size = new System.Drawing.Size(75, 25);
+            this.importBtn.TabIndex = 1;
+            this.importBtn.Text = "Import";
+            this.importBtn.UseVisualStyleBackColor = true;
+            this.importBtn.Click += new System.EventHandler(this.importBtn_Click);
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(464, 94);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(75, 25);
+            this.convertBtn.TabIndex = 2;
+            this.convertBtn.Text = "Convert";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(464, 149);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 3;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // statusTextBox
+            // 
+            this.statusTextBox.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusTextBox.Location = new System.Drawing.Point(24, 81);
+            this.statusTextBox.Multiline = true;
+            this.statusTextBox.Name = "statusTextBox";
+            this.statusTextBox.ReadOnly = true;
+            this.statusTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.statusTextBox.Size = new System.Drawing.Size(405, 161);
+            this.statusTextBox.TabIndex = 4;
+            this.statusTextBox.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Raw File:";
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(464, 218);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(75, 23);
+            this.btnAbout.TabIndex = 6;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 262);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusTextBox);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.importBtn);
+            this.Controls.Add(this.fileNameTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Raw2Array Converter";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Button importBtn;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.TextBox statusTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAbout;
+    }
+}
+
---
+++ MxtCfgConverter/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MxtCfgConverter
+{
+    public partial class MainForm : Form
+    {
+
+        private string fileName;
+        private StringBuilder cfgData = new StringBuilder();
+        private StreamReader sR;
+        private StreamWriter sW;
+
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void importBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+
+            openFileDlg.Title = "Choose File";
+            openFileDlg.Filter = "raw文件|*.raw";
+            openFileDlg.Multiselect = false;
+            openFileDlg.InitialDirectory = "D:\\";
+
+            if (openFileDlg.ShowDialog() == DialogResult.OK)
+            {
+
+                fileNameTextBox.Text = openFileDlg.FileName;
+
+            }
+
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+
+
+
+            if (fileNameTextBox.Text == "")
+            {
+
+                MessageBox.Show("请选择文件");
+
+            }
+            else
+            {
+
+                fileName = fileNameTextBox.Text;
+
+                cfgData.Clear();
+                sR = File.OpenText(fileName);
+                string nextLine;
+                int lineIndex = 0;
+
+                while ((nextLine = sR.ReadLine()) != null)
+                {
+                    //line index start from 1
+                    lineIndex++;
+
+                    if ((lineIndex == 1) && (nextLine != "OBP_RAW V1"))
+                    {
+                        MessageBox.Show("无效的文件格式");
+                    }
+                    else if (lineIndex == 2)
+                    {
+                        if (nextLine.Length != 20)
+                        {
+
+                        }
+                        else
+                        {
+
+                            string[] tmp = nextLine.Split(' ');
+                            for (int i = 0; i < tmp.Length; i++)
+                            {
+
+                                string local = "0x" + tmp[i] + ", ";
+                                cfgData.Append(local);
+
+                            }
+                            cfgData.Append("\r\n");
+                        }
+                    }
+                    else if ((lineIndex == 3) || (lineIndex == 4))
+                    {
+
+                        if (nextLine.Length != 6)
+                        {
+
+                        }
+                        else
+                        {
+
+                            for (int i = 0; i < 3; i++)
+                            {
+                                string t = "0x" + nextLine.Substring(2 * i, 2) + ", ";
+                                cfgData.Append(t);
+                            }
+
+                            cfgData.Append("\r\n");
+
+                        }
+                    }
+                    else if(lineIndex > 4){
+
+                        string[] tmp = nextLine.Split(' ');
+                        for (int i = 0; i < tmp.Length; i++) {
+
+                            if (i < 3) {
+                                string str = "0x" + tmp[i].Substring(0, 2) + ", ";
+                                cfgData.Append(str);
+                                str = "0x" + tmp[i].Substring(2, 2) + ", ";
+                                cfgData.Append(str);
+                            }
+                            else {
+                                cfgData.Append("0x" + tmp[i] + ", ");
+                            }
+                            
+
+                        }
+                        cfgData.Append("\r\n");
+
+                    }
+
+            statusTextBox.Text = cfgData.ToString();
+
+        }
+
+                sR.Close();
+    }
+
+ }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDlg = new SaveFileDialog();
+
+            saveFileDlg.Title = "Choose File";
+            saveFileDlg.Filter = "h文件|*.h";
+            saveFileDlg.FilterIndex = 1;
+            saveFileDlg.InitialDirectory = "D:\\";
+            saveFileDlg.RestoreDirectory = true;
+            
+
+            if (saveFileDlg.ShowDialog() == DialogResult.OK)
+            {
+
+                string saveFileName = saveFileDlg.FileName;
+                FileInfo saveFile = new FileInfo(saveFileName);
+                sW = saveFile.CreateText();
+
+                string prx = "#ifndef _ATMEL_MXT_CFG_H \r\n"+ "#define _ATMEL_MXT_CFG_H \r\n"+ "const unsigned char mxt_cfg_data[] = { \r\n";
+                string suf = "};\r\n" + "#endif";
+                sW.WriteLine(prx+cfgData+suf);
+                sW.Close();
+
+                MessageBox.Show("File Saved!\n"+saveFileName,"File Save Confirmation",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            string aboutText = "Raw2Array Converter, one of MXT Touch series tool, convert the Raw format configuration file to hex data array.\n\nVersion: V1.0.00\nAuthor: daviddong@solomon-systech.com\nDate: 2018-2-1";
+            MessageBox.Show(aboutText, "Raw to Array Converter", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
